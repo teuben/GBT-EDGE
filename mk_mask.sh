be916fd6c1fc9d07a0fd6ae8bc855ea1aef8353f
Peter Teuben <teuben@gmail.com>
2021-12-09 17:19:20 -0500

    better VSYS,since it's often not VREF

--- mk_mask.sh
+++ mk_mask.sh
@@ -3,20 +3,21 @@
 #  make a mask
 #     typical use:
 #
-#     ./mk_mask.sh refmap=NGC0001/NGC0001_12CO_rebase3_smooth2_hanning2.fits inc=45   pa=120  vsys=4485  mask=masks/mask_NGC0001.fits
+#     ./mk_mask.sh refmap=NGC0001/NGC0001_12CO_rebase3_smooth2_hanning2.fits  mask=masks/mask_NGC0001.fits 
+#     ./mk_mask.sh refmap=NGC0776/NGC0776_12CO_rebase3_smooth2_hanning2.fits  mask=masks/mask_NGC0776.fits  inc=46 pa=315 vsys=4830 v1=90
 #
-#
-#  two galaxies doesn't work yet, since this script forces vsys to be at the reference pixel
+#  - the defaults here are meant for NGC0001, the first galaxy we observed, and also a pretty good detection
+#  - two galaxies doesn't work yet, since this script forces vsys to be at the reference pixel
 
 refmap=masks/mask@0.010_NGC0001.fits
 refmap=NGC0001/NGC0001_12CO_rebase3_smooth2_hanning2.fits
 v1=350           # max rotation curve (vmax)
 r0=10            # radius where v1 (vmax)
 r1=30            # edge of galaxy
-inc=40      
-pa=120
+inc=42      
+pa=116
 vsys=4485
-sig=10           # measure of width of signal. will do 4 sigma
+sig=20           # measure of width of signal. will do 4 sigma
 nx=128
 ny=128
 nz=128
@@ -29,6 +30,8 @@ for arg in $*; do
   export $arg
 done
 
+# set -x
+
 if [ -z "$NEMO" ]; then
   echo This script needs NEMO
   exit 0
@@ -37,6 +40,11 @@ fi
 tmp=tmp
 
 rm -rf $tmp.* $mask
+
+# 0. The Rotation Center is going to be the crval of the refmap. We can't trust the vsys though
+  ra=$(fitshead $refmap | grep CRVAL1 | awk '{print $3}')
+ dec=$(fitshead $refmap | grep CRVAL2 | awk '{print $3}')
+vref=$(fitshead $refmap | grep CRVAL3 | awk '{print $3}')
 
 # 1. First make Density, Velocity and Sigma maps from 0 to r1.
 # ccdvel: there is a deprecated in=, or can it be used ?
@@ -49,7 +57,14 @@ ccdvel out=$tmp.s radii=0,$r0,$r1 vrot=100,100,50 inc=$inc pa=$pa size=$nx cell=
 #    note the refmap is in km/s
 velcube - $tmp.v $tmp.d zrange=${vsys}-5*${nz}:${vsys}+5*${nz} nz=$nz sigdefault=$sig |\
   ccdmath - -  'ifgt(%1,0,1,0)' |\
-  ccdfits - $mask refmap=$refmap refaxis=1,2,3 crpix=$nx/2,$nx/2,$nz/2 cdelt=-$cell/3600,$cell/3600,$chan 
+  ccdfits - $mask refmap=$refmap refaxis=1,2,3 \
+          crpix=$nx/2,$nx/2,$nz/2 \
+          cdelt=-$cell/3600,$cell/3600,$chan \
+          crval=$ra,$dec,$vsys
+
+# 4. Some reporting
+echo RA=$ra DEC=$dec VREF=$vref VSYS=$vsys
+
 # crval=1.8152157770653,27.70767526688,4419.5816198067
 # cunit=deg,deg,km/s
 

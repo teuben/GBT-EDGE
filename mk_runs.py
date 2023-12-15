17f8a8d6b31546e06cab0239c785536fb228f9ba
Peter Teuben <teuben@gmail.com>
2023-12-15 06:15:47 -0500

    remove duplicate

--- mk_runs.py
+++ mk_runs.py
@@ -88,8 +88,7 @@ for gal in gals.keys():
         fp.write('rm -rf %s/*_feed2_*\n' % gal)
         fp.write('./reduce.py %s -g %s  %s\n' % (m,tolist(ss),gal))
     fp.write('./mmaps.py %s\n' % gal)
-    fp.write('./plot_spectrum.py %s/%s%s savefig=%s/plot_spectrum.png\n' % (gal,gal,ext,gal))
-    fp.write('./mmaps.py %s\n' % gal)    
+    fp.write('./plot_spectrum.py %s/%s%s savefig=%s/plot_spectrum.png\n' % (gal,gal,ext,gal))    
     fp.close()
 print("Wrote %d run_GAL.sh scripts" % len(gals))
 

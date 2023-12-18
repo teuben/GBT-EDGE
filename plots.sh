fa4d123b9b1fb3417c1c05e61ea8724e3850d8ef
Peter Teuben <teuben@gmail.com>
2023-12-17 21:15:18 -0500

    typo

--- plots.sh
+++ plots.sh
@@ -1,3 +1,5 @@
+set -x
+
 # pars      # example
 gal=$1      #  NGC0001
 ext=$2      #  _12CO_rebase5_smooth1.3_hanning2.fits
@@ -10,6 +12,6 @@ slice=1:50,195-50:195
 ./plot_spectrum.py $cube size=10 vlsr=$vlsr savefig=$gal/plot_spectrum1.png
 ./plot_spectrum.py $cube size=30 vlsr=$vlsr savefig=$gal/plot_spectrum2.png
 fitsccd $cube - |                       ccdmom - - mom=-2 | ccdmath - - "%1*1000" | ccdfits - $gal/rms.fits
-fitsccd $fits - | ccdsub - - z=$slice | ccdmom - - mom=-2 | ccdmath - - "%1*1000" | ccdfits - $gal/rms1.fits
+fitsccd $cube - | ccdsub - - z=$slice | ccdmom - - mom=-2 | ccdmath - - "%1*1000" | ccdfits - $gal/rms1.fits
 ./fitsplot2.py $gal/rms.fits
 ./fitsplot2.py $gal/rms1.fits

105878ab849f8057952a20f80e88f6281782073f
Peter Teuben <teuben@gmail.com>
2024-02-21 08:20:41 -0500

    up through 48

--- run_NGC3406NED01.sh
+++ run_NGC3406NED01.sh
@@ -2,5 +2,5 @@
 # NGC3406NED01 [44, 44] vlsr=7411.6
 set -x
 rm -rf NGC3406NED01
-./reduce.py   -g 44 -f 2 NGC3406NED01
+./reduce.py   -g 44 -f 2,12 NGC3406NED01
 ./plots.sh NGC3406NED01 _12CO_rebase5_smooth1.3_hanning2.fits 7411.6
--- run_NGC3614.sh
+++ run_NGC3614.sh
@@ -1,6 +1,7 @@
 # Created by mk_runs.py
-# NGC3614 [14, 14] vlsr=2305.6
+# NGC3614 [14, 14, 48, 48, 48] vlsr=2305.6
 set -x
 rm -rf NGC3614
 ./reduce.py -m mask_NGC3614_Havfield_v1.fits -g 14 NGC3614
+./reduce.py -m mask_NGC3614_Havfield_v1.fits -g 48 -f 2 NGC3614
 ./plots.sh NGC3614 _12CO_rebase5_smooth1.3_hanning2.fits 2305.6

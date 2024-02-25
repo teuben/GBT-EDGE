21ad630b625c8ac02ef3cbb383bf8b8cadb01b3b
Peter Teuben <teuben@gmail.com>
2024-02-25 09:43:31 -0500

    add session 50

--- run_UGC10097.sh
+++ run_UGC10097.sh
@@ -1,5 +1,5 @@
 # Created by mk_runs.py
-# UGC10097 [23, 23, 49, 49, 49] vlsr=5943.7
+# UGC10097 [23, 23, 49, 49] vlsr=5943.7
 set -x
 rm -rf UGC10097
 ./reduce.py -m mask_UGC10097_Havfield_v1.fits -g 23 UGC10097
--- run_UGC9837.sh
+++ run_UGC9837.sh
@@ -1,6 +1,7 @@
 # Created by mk_runs.py
-# UGC9837 [24, 24] vlsr=2682.9
+# UGC9837 [24, 24, 50, 50, 50, 50] vlsr=2682.9
 set -x
 rm -rf UGC9837
 ./reduce.py -m mask_UGC9837_Havfield_v1.fits -g 24 UGC9837
+./reduce.py -m mask_UGC9837_Havfield_v1.fits -g 50 -f 2 UGC9837
 ./plots.sh UGC9837 _12CO_rebase5_smooth1.3_hanning2.fits 2682.9

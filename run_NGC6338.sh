449f95ccb5de5f3dc41a3e2523a4428437243f6f
Peter Teuben <teuben@gmail.com>
2024-03-24 07:36:03 -0400

    new

---
+++ run_NGC6338.sh
@@ -0,0 +1,6 @@
+# Created by mk_runs.py
+# NGC6338 [52, 52] vlsr=8170.7
+set -x
+rm -rf NGC6338
+./reduce.py   -g 52 -f 2 NGC6338
+./plots.sh NGC6338 _12CO_rebase5_smooth1.3_hanning2.fits 8170.7
---
+++ run_NGC6497.sh
@@ -0,0 +1,6 @@
+# Created by mk_runs.py
+# NGC6497 [52, 52] vlsr=6021.0
+set -x
+rm -rf NGC6497
+./reduce.py   -g 52 -f 2 NGC6497
+./plots.sh NGC6497 _12CO_rebase5_smooth1.3_hanning2.fits 6021.0

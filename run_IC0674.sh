4e75db90cf4da4901062c48c16287de26c7fcfbd
Peter Teuben <teuben@gmail.com>
2024-04-14 17:00:12 -0400

    52 and 53

--- run_IC0674.sh
+++ run_IC0674.sh
@@ -1,7 +1,7 @@
 # Created by mk_runs.py
-# IC0674 [18, 18, 34, 34] vlsr=7497.7
+# IC0674 [18, 18, 34, 34, 53, 53] vlsr=7497.7
 set -x
 rm -rf IC0674
 ./reduce.py -m mask_IC0674_Havfield_v1.fits -g 18 IC0674
-./reduce.py -m mask_IC0674_Havfield_v1.fits -g 34 -f 2 IC0674
+./reduce.py -m mask_IC0674_Havfield_v1.fits -g 34,53 -f 2 IC0674
 ./plots.sh IC0674 _12CO_rebase5_smooth1.3_hanning2.fits 7497.7

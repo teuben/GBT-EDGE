531acb8fbda70d0c01b5132ec63f1d7cfc57b014
Peter Teuben <teuben@gmail.com>
2024-11-13 01:44:26 -0500

    session 57

--- run_UGC04258.sh
+++ run_UGC04258.sh
@@ -1,6 +1,6 @@
 # Created by mk_runs.py
-# UGC04258 [2, 2] vlsr=3097.7
+# UGC04258 [2, 2, 55, 55, 55] vlsr=3097.7
 set -x
 rm -rf UGC04258
-./reduce.py -m mask_UGC04258_Havfield_v1.fits -g 2 UGC04258
+./reduce.py -m mask_UGC04258_Havfield_v1.fits -g 2,55 UGC04258
 ./plots.sh UGC04258 _12CO_rebase5_smooth1.3_hanning2.fits 3097.7

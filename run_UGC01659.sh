baf0ccc376a7a15bfa446560070f899444548adf
Peter Teuben <teuben@gmail.com>
2024-11-18 04:55:11 -0500

    add nov 12/13

--- run_UGC01659.sh
+++ run_UGC01659.sh
@@ -1,6 +1,6 @@
 # Created by mk_runs.py
-# UGC01659 [1, 1, 55, 55] vlsr=8195.4
+# UGC01659 [1, 1, 55, 55, 58, 58] vlsr=8195.4
 set -x
 rm -rf UGC01659
-./reduce.py -m mask_UGC01659_Havfield_v1.fits -g 1,55 UGC01659
+./reduce.py -m mask_UGC01659_Havfield_v1.fits -g 1,55,58 UGC01659
 ./plots.sh UGC01659 _12CO_rebase5_smooth1.3_hanning2.fits 8195.4
--- run_UGC01938.sh
+++ run_UGC01938.sh
@@ -1,6 +1,6 @@
 # Created by mk_runs.py
-# UGC01938 [56, 56] vlsr=6285.3
+# UGC01938 [56, 56, 58] vlsr=6285.3
 set -x
 rm -rf UGC01938
-./reduce.py   -g 56 UGC01938
+./reduce.py   -g 56,58 UGC01938
 ./plots.sh UGC01938 _12CO_rebase5_smooth1.3_hanning2.fits 6285.3
